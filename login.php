9d70e70f485e31bea1a1da4bebf6f71c468b651d
Raul Gabriel <148444610+Raaul00@users.noreply.github.com>
2024-05-23 12:11:21 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,32 +5,39 @@ $ldap_port = 389; // Change this to your LDAP server port
 $ldap_base_dn = 'dc=projecte,dc=org'; // Change this to your LDAP base DN
 $ldap_user_suffix = '@projecte.org'; // Change this to your LDAP user suffix
 $ldap_group_dn = 'cn=usuaris,dc=projecte,dc=org'; // Change this to your LDAP group DN if needed
+echo $ldap_host;
+echo $ldap_port;
+echo $ldap_base_dn;
+
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Get form inputs
     $username = $_POST['username'];
     $password = $_POST['password'];
-
+    #$md5Hash = md5($password);
+    echo $username . ' : ' . $password;
     // LDAP connection
-    $ldap_conn = ldap_connect($ldap_host, $ldap_port);    
+    $ldap_conn = ldap_connect($ldap_host, $ldap_port);	
     if ($ldap_conn) {
         // Set LDAP options
         ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
         ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);
-
+        ldap_set_option($ldap_conn, LDAP_OPT_DEBUG_LEVEL, 7);
+    
         // Bind to LDAP server
-        $ldap_bind = ldap_bind($ldap_conn, 'cn=' . $username . ',' . $ldap_base_dn, $password);
+        #$ldap_bind = @ldap_bind($ldap_conn, $username . $ldap_user_suffix, $password);
+	#$ldap_bind = ldap_bind($ldap_conn, 'cn=' . $username . ',' . $ldap_base_dn, $md5Hash);
+	$ldap_bind = ldap_bind($ldap_conn, 'cn=' . $username . ',' . $ldap_base_dn, $password);
 
         if ($ldap_bind) {
             // User authenticated
-            // Redirigir a perfil.html
+            echo "Login successful!";
             header("Location: perfil.html");
-            exit; // Asegura que el script no continúe después de la redirección
+            exit;
         } else {
             // Authentication failed
-            // Permanece en login.html
-            header("Location: login.html");
+            echo "<script>alert('Usuari o contrasenya incorrecte');window.location.href = 'login.html';</script>";
             exit;
         }
     } else {
@@ -40,4 +47,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Close LDAP connection
     ldap_close($ldap_conn);
 }
+?>
+
+    ldap_close($ldap_conn);
+//}
+echo "adeu!!!";
 ?>
